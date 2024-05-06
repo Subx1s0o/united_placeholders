3393583bb4baf8b05932bf9da72d21ef834ffe75
Subx1s0o <nazarsabardin@gmail.com>
2024-05-06 16:15:26 +0300

    add adjustments

--- src/main.js
+++ src/main.js
@@ -10,3 +10,27 @@ modalBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   modal.classList.remove('is-open');
 });
+document.querySelectorAll('.form-input').forEach(input => {
+  input.addEventListener('input', function () {
+    const isValidInput = this.validity.valid;
+    const validIndicator = this.parentElement.querySelector('.valid-indicator');
+
+    if (this.value.trim().length >= this.getAttribute('minlength')) {
+      validIndicator.classList.add('show');
+    } else {
+      validIndicator.classList.remove('show');
+    }
+
+    if (this.value.trim() && isValidInput) {
+      validIndicator.classList.remove('invalid');
+      validIndicator.classList.add('valid');
+      validIndicator.innerHTML =
+        '<svg width="7" height="7"><use href="img/icons.svg#icon-checkbox"></use></svg>';
+    } else {
+      validIndicator.classList.remove('valid');
+      validIndicator.classList.add('invalid');
+      validIndicator.innerHTML =
+        '<svg width="7" height="7"><use href="img/icons.svg#icon-close"></use></svg>';
+    }
+  });
+});
